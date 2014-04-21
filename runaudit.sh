4e6a8e2630d0aa7f17bd1d0331f2abceaa317286
swl09 <steve.litras@autodesk.com>
2014-04-21 16:54:07 -0700

    Cleaned up Script a little bit...

--- runaudit.sh
+++ runaudit.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-instance=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
+#####
+# A simple script, intended to be run as userdata on a new instance, that runs a couple aws cli commands 
+# to get information about the current account, formats it into a single JSON file, posts it to an S3 
+# bucket, and then terminates the instance it's running on. 
+#
+# Written by Steven W. Litras, Copyright 2014 Autodesk, Inc.
+#####
 
+### 
+# Configurables
+###
+s3bucket="adsk-eis-ea-audit"
+
+###
+# Runtime Variables pulled from AWS data...
+###
+instance=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
 objname=`aws iam list-account-aliases | head -3 | tail -1 | awk -F\" '{print $2}'`
 filename=/var/tmp/$objname
+
+###
+# Execution
+###
 echo $filename
 echo "{" > $filename
 echo "\"account\": \"$objname\"," >> $filename
@@ -30,7 +49,7 @@ done
 echo "}" >> $filename
 
 
-aws s3api put-object --acl bucket-owner-full-control --bucket adsk-eis-ea-audit --key ${objname}.txt --body $filename
+aws s3api put-object --acl bucket-owner-full-control --bucket $bucket --key ${objname}.txt --body $filename
 #aws s3api put-object --bucket adsk-eis-ea-audit --key ${objname}.txt --body $filename
 
 echo "Right now, I would TOTALLY terminate $instance"
